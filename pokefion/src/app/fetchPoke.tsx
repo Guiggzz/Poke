8e170d036bac1b112df1f83e81c7bec2d02d5acd
Guiggzz <guillaume.breon@gmail.com>
2025-03-24 16:06:57 +0100

    dlt old files

--- pokefion/src/app/fetchPoke.tsx
+++
@@ -1,9 +0,0 @@
-async function fetchPoke() {
-    const response = await fetch("https://pokedex-api.3rgo.tech/api/pokemon");
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement des données");
-    }
-    return response.json();
-}
-
-export { fetchPoke };
--- pokefion/src/app/fetchType.tsx
+++
@@ -1,9 +0,0 @@
-async function fetchType() {
-    const response = await fetch("https://pokedex-api.3rgo.tech/api/types");
-    if (!response.ok) {
-        throw new Error("Erreur lors du chargement des données");
-    }
-    return response.json();
-}
-
-export { fetchType };
